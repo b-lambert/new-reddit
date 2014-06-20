3be0594e3975f4ec363da7d59620a1eb7c0e70e9
blahshaw <benjaminhlambert@gmail.com>
2014-06-20 01:41:12 -0700

    added maxlength to name/url columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140620075928) do
+ActiveRecord::Schema.define(version: 20140620080539) do
 
   create_table "posts", force: true do |t|
     t.string   "name"
